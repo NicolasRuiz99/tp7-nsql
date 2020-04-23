f45fd172803f12438cc91d00e1aa99ad3bc11c23
Nicolás Ruiz <nicoruiz72@gmail.com>
2020-04-22 21:18:52 -0300

    comienzo carga de bbdd

--- backend/connectiondb.py
+++ backend/connectiondb.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+import json
 
 def inicializar_db ():
     try:
@@ -8,3 +9,13 @@ def inicializar_db ():
         return mydb
     except (Exception) as err:
         return err
+
+with open('currencies.json') as f:
+    file_data = json.load(f)
+
+# if pymongo < 3.0, use insert()
+collection_currency.insert(file_data)
+# if pymongo >= 3.0 use insert_one() for inserting one document
+collection_currency.insert_one(file_data)
+# if pymongo >= 3.0 use insert_many() for inserting many documents
+collection_currency.insert_many(file_data)
